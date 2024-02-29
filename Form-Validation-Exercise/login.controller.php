334cf955b21b738dccbd55b07da7cb98bb1c9ea9
XPERIA679 <sorianorobin38gmail.com>
2024-02-29 10:20:24 +0800

    First Commit of Exercise 5

---
+++ Form-Validation-Exercise/login.controller.php
@@ -0,0 +1,70 @@
+<?php
+
+require_once 'login.model.php';
+
+class LoginController {
+    private $userModel;
+    private $data;
+    private $errors = [];
+    private static $fields = ['username', 'password'];
+
+    public function __construct($post_data) {
+        $this->data = $post_data;
+        $this->userModel = new User();
+    }
+
+    public function handleLogin() {
+        $result = ['success' => false, 'message' => ''];
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+
+            if ($this->userModel->validateCredentials($username, $password)) {
+                $result['success'] = true;
+            } else {
+                $result['message'] = 'Invalid credentials. Please try again.';
+            }
+            
+        }
+
+        return $result;
+    }
+
+    public function validateForm() {
+        foreach (self::$fields as $field) {
+            if (!isset($this->data[$field])) {
+                // trigger_error("$field is not present in data");
+                return;
+            }
+        }
+    
+        $this->validateUsername();
+        $this->validatePassword();
+        return $this->errors;
+    }
+
+    private function validateUsername() {
+
+        $val = trim($this->data['username']);
+
+        if(empty($val)) {
+            $this->addError('username', 'username cannot be empty'); 
+        }
+
+    }
+
+    private function validatePassword() {
+
+        $val = trim($this->data['password']);
+
+        if(empty($val)) {
+            $this->addError('password', 'password cannot be empty'); 
+        }
+
+    }
+
+    private function addError($key, $val) {
+        $this->errors[$key] = $val;
+    }
+}
